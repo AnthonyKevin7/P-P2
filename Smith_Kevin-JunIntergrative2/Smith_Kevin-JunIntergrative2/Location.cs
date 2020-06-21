ee4bc415eab0e3fbc3d53598aaecbbae4ffd3423
Kevin Smith <Kasmith4@student.fullsail.edu>
2020-06-21 16:18:33 -0400

    Added JSON and Star ratings

---
+++ Smith_Kevin-JunIntergrative2/Smith_Kevin-JunIntergrative2/Location.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Smith_Kevin_JunIntergrative2
+{
+    public class Location
+    {
+        public Location()
+        {
+        }
+    }
+}
---
+++ Smith_Kevin-JunIntergrative2/Smith_Kevin-JunIntergrative2/Logic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Smith_Kevin_JunIntergrative2
+{
+    public class Logic
+    {
+        public Logic()
+        {
+        }
+    }
+}
